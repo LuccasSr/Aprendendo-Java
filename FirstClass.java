f4bb219742dd2a0591f573abfd07e4e096ddaa8f
LuccasSr <djluccasdaquebrada@gmail.com>
2024-07-10 12:07:07 -0300

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,14 @@
+
+public class FirstClass {
+    public static void main(String[] args)  {
+    
+    
+        System.out.print("Hello, Tim!");
+
+
+        
+    }
+
+}
+
+
---
+++ SecondClass.java
@@ -0,0 +1,30 @@
+public class SecondClass{
+    public static void main(String[] args) {
+        //ystem.out.println("Fuck World");
+        //System.out.println("Ta ok");
+        
+    //     boolean mybo = true;
+    //     if(mybo == true){
+    //         System.out.println("Nao é falso");
+
+    //     }
+
+    //     else{
+    //         System.out.println("E verdade");
+    //     }
+    String myString = "Hello World";
+    if(myString.equals("Hello World" )){
+        System.out.println("Tudo certo");
+    }
+
+
+    int myNewInt = 80;
+    if(myNewInt <=100){
+       // System.out.println("Sua variavel é menor que 100");
+    }
+    int secondInt = 60;
+    if(myNewInt > secondInt && myNewInt == 80){
+        System.out.println("A sua primeira variavel continua sendo a maior");
+    }
+}
+}
